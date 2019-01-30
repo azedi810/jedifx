f8c1165c40be3b183858fa7c19d90d6fb31f0c2c
hpnotes <hpnotes@hpnote>
2019-01-31 00:13:31 +0900

    학원 인강

--- 이론/기타/자격증/041_학원_인강.sql
+++ 이론/기타/자격증/041_학원_인강.sql
@@ -8,3 +8,8 @@
  ******************************************************/
  1. 내일배움카드 신청
  2. 근로자 훈련과정
+
+ 
+ https://www.airklass.com/v/DZTZQ6D
+	 https://www.airklass.com/pr/promotion?id=65&gclid=EAIaIQobChMIwJfnqM-V4AIVxQQqCh29GgIoEAMYASAAEgK_g_D_BwE
+https://www.lyzeum.com/board/summary.asp?Boar_code=book&Clas_Code=CC1364
